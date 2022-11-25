f81e50d0bed141efcf4368161afc4daaf28ff17b
Alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-11-25 16:40:31 -0600

    save old page

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<link rel="icon" type="image/png" href="/imagen/favicons.png" />
+		<title>Inicio CSP</title>
+
+</head>
+
+<body>
+<div align="center">
+  <table width="912" border="0">
+    <tr>
+      <td width="110"><div align="center"><img src="../../imagen/2018_cdmx.svg" width="110" height="108" /></div></td>
+      <td width="200">&nbsp;</td>
+      <td width="270">&nbsp;</td>
+      <td width="200">&nbsp;</td>
+      <td width="110"><div align="center"><img src="../../imagen/cejur.jpg" alt="CEJUR" width="96.4" height="95" /></div></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>		<div style="text-align: center"><h1><a href="http://10.4.3.122/bitacora/"
+			>Bitacora Electronica</a></h1></div>
+	  <br /></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="21">&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>		<div style="text-align: center"><h3><a href="ftp://10.4.3.122/"
+			>Archivo de <br />Dictamenes e informes<br /></a>
+		Requiere contrasena </div></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>		<div align="center"><a href="http://10.4.3.122/pruebas/calendario/index.php?formulario=fcalen&nomcampo=fecha1"
+			> <img src="../../imagen/cal.jpg" width="50" height="50" /><br />Calendario</a></div></td>
+      <td><p>
+           <a href="ftp://10.4.3.122/archivos/20190607_lcccdmx.pdf" title="Ley de Cultura Civica" target="_blank">Ley de Cultura Civica</a><br />
+           <a href="ftp://10.4.3.122/archivos/rlccdf.pdf" title="Reglamento de la Ley de Cultura Civica" target="_blank">Reglamento de la Ley CC</a><br />
+           <a href="ftp://10.4.3.122/archivos/reglamentotracdmx.pdf" title="Nuevo Reglamento de Tránsito Metropolitano" target="_blank">Reglamento de Tránsito Metropolitano</a><br />
+           <a href="ftp://10.4.3.122/archivos/protocolo.pdf" title="Gaceta publicada" target="_blank">Protocolo de Actuacion de PTT</a></td>
+      <td>		<div align="center"><a href="/connection/consultasql.php">
+        <img src="../../imagen/dsql.gif" width="112" height="66" /><br />
+        Consultas SQL a CSP</a></div></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>La informacion esta almacenada en el servidor de CSP.</td>
+      <td>
+	    <div align="center">
+	      <p><a href="http://10.4.3.122/ayuda"><img src="../../imagen/salvavida.jpg" width="75" height="57" /><br /></a><a href="http://10.4.3.122/ayuda">Ayuda ? </a> </p>
+        </div></td>
+      <td><p>Favor de verificar que el servidor este encendido.</p>
+      </td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td><div align="center"><img src="../../imagen/earmasdf.jpg" alt="Escudo de Armas" width="58" height="58" /></div></td>
+      <td>&nbsp;</td>
+      <td><div align="center">
+        serviciospericialesdejc@yahoo.com
+        <br /><?php echo 'CSPS ', $_SERVER['SERVER_NAME']; ?><br />
+        
+      </div></td>
+      <td>&nbsp;</td>
+      <td><div align="center"><img src="../../imagen/jc.jpg" alt="Justicia Civica" width="68" height="71" /></div></td>
+    </tr>
+  </table>
+</div>
+</body>
+</html>
