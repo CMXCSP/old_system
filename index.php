f8eec2491fc17947c45d9b062fd0dbcd3248acfb
Alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-11-25 16:55:31 -0600

    save images

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="es">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-	<link rel="icon" type="image/png" href="/imagen/favicons.png" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="/imagen/favicons.png" />
 		<title>Inicio CSP</title>
 
 </head>
