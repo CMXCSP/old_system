07a8f07fd8b903f9b7e4eb715a37d343bfc235fa
Alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-11-25 18:04:13 -0600

    add gitignore

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="/imagen/favicons.png" />
+    <link rel="icon" type="image/png" href="./imagen/favicons.png" />
 		<title>Inicio CSP</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
@@ -12,17 +12,17 @@
 
 <header>
     <figure>
-      <img src="imagen/2018_cdmx.svg" alt="logo CDMX 2018" height="80">
+      <img src="./imagen/2018_cdmx.svg" alt="logo CDMX 2018" height="80">
       <figcaption>CDMX</figcaption>
     </figure>
     <figure>
-      <img src="imagen/cejur.jpg" alt="logo CEJUR 2018" height="80">
+      <img src="./imagen/cejur.jpg" alt="logo CEJUR 2018" height="80">
       <figcaption>CEJUR</figcaption>
     </figure> 
 </header>
 
 <main>
-  <h1><a href="bitacora/">Bitacora Electronica</a></h1>
+  <h1><a href="./bitacora/">Bitacora Electronica</a></h1>
 
   <h3>
   <a href="ftp://10.4.3.122/">Archivo de Dictamenes e informes</a>
@@ -30,8 +30,8 @@
   </h3>
 
   <figure>
-  <a href="pruebas/calendario/index.php?formulario=fcalen&nomcampo=fecha1">
-  <img src="imagen/cal.jpg" width="50"/>
+  <a href="./pruebas/calendario/index.php?formulario=fcalen&nomcampo=fecha1">
+  <img src="./imagen/cal.jpg" width="50"/>
   <figcaption>Calendario</figcaption>
   </a>
   </figure>
@@ -43,7 +43,7 @@
   <a href="ftp://10.4.3.122/archivos/protocolo.pdf" title="Gaceta publicada" target="_blank">Protocolo de Actuacion de PTT</a>
   <a href="/connection/consultasql.php">
   <figure>
-  <img src="imagen/dsql.gif" width="112"/>
+  <img src="./imagen/dsql.gif" width="112"/>
   <figcaption>Consultas SQL a CSP</figcaption>
   </figure>
   </section>
@@ -52,8 +52,8 @@
   <p>La informacion esta almacenada en el servidor de CSP.</p>
 
   <figure>
-  <a href="ayuda/">
-  <img src="imagen/salvavida.jpg" width="75"/>
+  <a href="./ayuda/">
+  <img src="./imagen/salvavida.jpg" width="75"/>
   <figcaption>Ayuda ? </figcaption>
   </a>
   </figure>
@@ -63,7 +63,7 @@
   <p>Favor de verificar que el servidor este encendido.</p>
 
   <figure>
-  <img src="imagen/earmasdf.jpg" alt="Escudo de Armas" width="58"/>
+  <img src="./imagen/earmasdf.jpg" alt="Escudo de Armas" width="58"/>
   </figure>
 
   <address>
@@ -72,7 +72,7 @@
   </address>
 
   <figure>
-  <img src="imagen/jc.jpg" alt="Justicia Civica" width="68"/>
+  <img src="./imagen/jc.jpg" alt="Justicia Civica" width="68"/>
   </figure>
 </footer>
 
